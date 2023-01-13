779a4d2ccf31dbc816b26c2e1914fff66ca67f88
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2023-01-13 19:52:43 +0100

    Use protobuf as package

--- demo/build.sh
+++ demo/build.sh
@@ -11,8 +11,10 @@ osmium extract --bbox=20.6,51.8,21.5,52.6 poland-latest.osm.pbf -o warsaw.osm.pb
 popd
 
 docker build -t mama-demo -f ../server/Dockerfile .. 
-docker run -v $(pwd)/tiles:/tiles -t mama-demo  /app/tilegen /tiles/warsaw.osm.pbf /tiles
-docker run -v $(pwd)/tiles:/tiles -t mama-demo  /app/mama_server
+docker run -v $(pwd)/tiles:/tiles -t mama-demo /app/tilegen /tiles/warsaw.osm.pbf /tiles
+echo "RUNNING mama_server"
+docker run -v $(pwd)/tiles:/tiles -t mama-demo /app/mama_server
+echo "STOPPED mama_server"
 
 # # run server
 # docker compose build
