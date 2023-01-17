38c2d376d1dd916ad84d70736d2a22a924856fd8
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2023-01-17 17:56:55 +0100

    Fix issues in mama_server docker image (#8)

---
+++ demo/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# generate tiles
+rm -rf tiles
+mkdir tiles 
+pushd tiles
+wget -q http://download.geofabrik.de/europe/poland-latest.osm.pbf
+# extract Warsaw area using osmium (`brew install osmium-tool` or `apt-get install osmium-tool`)
+osmium extract --bbox=20.6,51.8,21.5,52.6 poland-latest.osm.pbf -o warsaw.osm.pbf
+popd
+
+# build mama server
+docker build -t mama-demo -f ../server/Dockerfile .. 
+
+# generate tiles
+docker run -v $(pwd)/tiles:/tiles -t mama-demo /app/tilegen /tiles/warsaw.osm.pbf /tiles
+
+# build demo
+docker compose build
