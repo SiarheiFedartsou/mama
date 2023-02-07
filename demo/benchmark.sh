59264856ecc008555b2fffcf88b4d41aeff38048
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2023-02-07 18:45:00 +0100

    wip

--- demo/benchmark.sh
+++ demo/benchmark.sh
@@ -1,1 +1,17 @@
-ghz --total 100000 --insecure --proto ./server/mama.proto --call mama.server.api.MamaService.Match --data '{"entries": [{"location": {"longitude": 7.41795, "latitude": 43.73247}}]}' localhost:50051
+#!/bin/bash
+set -e
+
+# install ghz
+curl https://github.com/bojand/ghz/releases/download/v0.114.0/ghz-linux-x86_64.tar.gz | tar -xz
+mv ghz /usr/local/bin
+
+
+# TODO: kill container afterwards
+docker run --detached -p 50051:50051 -v $(pwd)/tiles:/tiles -t mama-server /tiles
+
+# TODO: add normal healthcheck
+sleep 5
+
+# TODO: we should also benchmark case when we have previous state
+ghz --total 1000000 --insecure --proto ./server/mama.proto --call mama.server.api.MamaService.Match --data '{"entries": [{"location": {"longitude": 21.0245, "latitude": 52.2441}}]}' localhost:50051
+
