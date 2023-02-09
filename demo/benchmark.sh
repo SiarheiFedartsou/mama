29bc19a746887ebf614065daf0445a7f9861fd89
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2023-02-09 17:55:26 +0100

    wip

--- demo/benchmark.sh
+++ demo/benchmark.sh
@@ -2,7 +2,7 @@
 set -e
 
 # install ghz
-curl https://github.com/bojand/ghz/releases/download/v0.114.0/ghz-linux-x86_64.tar.gz | tar -xz
+curl -L https://github.com/bojand/ghz/releases/download/v0.114.0/ghz-linux-x86_64.tar.gz | tar xvz
 mv ghz /usr/local/bin
 
 
@@ -10,7 +10,7 @@ mv ghz /usr/local/bin
 docker run --detached -p 50051:50051 -v $(pwd)/tiles:/tiles -t mama-server /tiles
 
 # TODO: add normal healthcheck
-sleep 5
+sleep 10
 
 # TODO: we should also benchmark case when we have previous state
 ghz --total 1000000 --insecure --proto ./server/mama.proto --call mama.server.api.MamaService.Match --data '{"entries": [{"location": {"longitude": 21.0245, "latitude": 52.2441}}]}' localhost:50051
