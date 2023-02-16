a18cf6846fc52756df0ab5ab4f634b071995e7e8
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2023-02-16 17:47:29 +0100

    wip

--- demo/benchmark.sh
+++ demo/benchmark.sh
@@ -3,16 +3,26 @@ set -e
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-# install ghz
+install ghz
 curl -L https://github.com/bojand/ghz/releases/download/v0.114.0/ghz-linux-x86_64.tar.gz | tar xvz
 mv ghz /usr/local/bin
 
-CONTAINER_ID=$(docker run -d -p 50051:50051 -v $SCRIPTPATH/tiles:/tiles -it mama-server mama_server /tiles)
+CONTAINER_ID=$(docker run -d -p 50051:50051 -v $SCRIPTPATH/tiles:/tiles -it mama-server /app/mama_server /tiles)
+
+trap "docker kill $CONTAINER_ID" EXIT
 
 # TODO: add normal healthcheck
-sleep 3
+sleep 10
 
-# TODO: we should also benchmark case when we have previous state
-ghz --total 100000 --insecure --proto $SCRIPTPATH/protos/mama.proto --call mama.server.api.MamaService.Match --data '{"entries": [{"location": {"longitude": 21.0245, "latitude": 52.2441}}]}' localhost:50051
+# without state
+DATA='{"entries": [{"location": {"longitude": 21.0245, "latitude": 52.2441}}]}'
+echo "Results without state:"
+ghz --total 100000 --insecure --proto $SCRIPTPATH/protos/mama.proto --call mama.server.api.MamaService.Match --data "$DATA" localhost:50051
 
-docker kill $CONTAINER_ID
+
+# with state
+STATE=$(node $SCRIPTPATH/benchmark_get_state.js 21.1245 52.2441)
+DATA="{\"entries\": [{\"state\": \"${STATE}\", \"location\": {\"longitude\": 21.0245, \"latitude\": 52.2441}}]}"
+
+echo "Results with state:"
+ghz --total 100000 --insecure --proto $SCRIPTPATH/protos/mama.proto --call mama.server.api.MamaService.Match --data "$DATA" localhost:50051
