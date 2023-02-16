48fccdc1db7c06f08a4db41323e0941f04623f2a
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2023-02-16 17:50:34 +0100

    wip

--- demo/benchmark.sh
+++ demo/benchmark.sh
@@ -4,7 +4,7 @@ set -e
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 install ghz
-curl -L https://github.com/bojand/ghz/releases/download/v0.114.0/ghz-linux-x86_64.tar.gz | tar xvz
+curl -sSL https://github.com/bojand/ghz/releases/download/v0.114.0/ghz-linux-x86_64.tar.gz | tar xvz
 mv ghz /usr/local/bin
 
 CONTAINER_ID=$(docker run -d -p 50051:50051 -v $SCRIPTPATH/tiles:/tiles -it mama-server /app/mama_server /tiles)
